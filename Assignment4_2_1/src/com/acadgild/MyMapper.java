554e05e99cf400a5b7dd8cd922be8393fee01038
Sai kishore Amirishetty <sai.kishi2010@gmail.com>
2016-10-01 21:37:05 +0530

    Two problems Two folders

---
+++ Assignment4_2_1/src/com/acadgild/MyMapper.java
@@ -0,0 +1,24 @@
+package com.acadgild;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class MyMapper extends Mapper<LongWritable,Text,Text,IntWritable>{
+
+	public void map(LongWritable key,Text value,Context context) throws IOException, InterruptedException{
+		
+		String line=value.toString();
+		String[] st=line.split("\n");
+	
+		for(int i=0;i<st.length;i++){
+			String[] st1=st[i].split("\\|");
+			if(st1[0].equals("Onida"))
+			context.write(new Text(st1[3]), new IntWritable(1));
+			
+		}
+	}
+}
